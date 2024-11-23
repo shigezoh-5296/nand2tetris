43397a29d11cd10c19ef8c794993ad6f289c2dce
koby0203 <ms23opboc@gmail.com>
2024-11-23 12:57:42 +0900

    7章 ステージ#1 eq/gt/ltまで 実装完了(テスト未実施)

--- projects/7/src/code_writer.py
+++ projects/7/src/code_writer.py
@@ -4,8 +4,9 @@ import os
 class CodeWriter:
     def __init__(self, filename, directory):
         self.output_filename = filename
-        # asm_directory = os.path.join(directory, 'asm')
-        # os.makedirs(asm_directory, exist_ok=True)
+        self.eq_count = 0
+        self.gt_count = 0
+        self.lt_count = 0
         try:
             self.f = open(os.path.join(directory, filename + '.asm'), 'w')
         except IOError as e:
@@ -19,44 +20,110 @@ class CodeWriter:
         asm_code = []
         if command == 'add':
             # add                       Ex) RAM[0]=SP=258, RAM[257]=2, RAM[256]=3
-            asm_code.append('@SP')      # A=0,   M=RAM[0],   D=0,  RAM[0]=258, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('M=M-1')    # A=0,   M=RAM[0],   D=0,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('A=M')      # A=257, M=RAM[257], D=0,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('D=M')      # A=257, M=RAM[257], D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('@SP')      # A=0,   M=RAM[0],   D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('M=M-1')    # A=0,   M=RAM[0],   D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('A=M')      # A=256, M=RAM[256], D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('M=D+M')    # A=256, M=RAM[256], D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=5
-            asm_code.append('@SP')      # A=0,   M=RAM[0],   D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=5
-            asm_code.append('M=M+1')    # A=0,   M=RAM[0],   D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=5
+            asm_code.extend([
+                '@SP',      # A=0,   M=RAM[0],   D=0,  RAM[0]=258, RAM[258]=0, RAM[257]=2, RAM[256]=3
+                'AM=M-1',   # A=257, M=RAM[257], D=0,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
+                'D=M',      # A=257, M=RAM[257], D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
+                '@SP',      # A=0,   M=RAM[0],   D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
+                'AM=M-1',   # A=256, M=RAM[256], D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=3
+                'M=D+M',    # A=256, M=RAM[256], D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=5
+                '@SP',      # A=0,   M=RAM[0],   D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=5
+                'M=M+1'     # A=0,   M=RAM[0],   D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=5
+            ])
         elif command == 'sub':
-            # sub                       Ex) RAM[0]=SP=258, RAM[257]=2, RAM[256]=3
-            asm_code.append('@SP')      # A=0,   M=RAM[0],   D=0,  RAM[0]=258, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('M=M-1')    # A=0,   M=RAM[0],   D=0,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('A=M')      # A=257, M=RAM[257], D=0,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('D=M')      # A=257, M=RAM[257], D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('@SP')      # A=0,   M=RAM[0],   D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('M=M-1')    # A=0,   M=RAM[0],   D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('A=M')      # A=256, M=RAM[256], D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=3
-            asm_code.append('M=M-D')    # A=256, M=RAM[256], D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=1
-            asm_code.append('@SP')      # A=0,   M=RAM[0],   D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=1
-            asm_code.append('M=M+1')    # A=0,   M=RAM[0],   D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=1
+            # sub           Ex) RAM[0]=SP=258, RAM[257]=2, RAM[256]=3
+            asm_code.extend([
+                '@SP',      # A=0,   M=RAM[0],   D=0,  RAM[0]=258, RAM[258]=0, RAM[257]=2, RAM[256]=3
+                'AM=M-1',   # A=257, M=RAM[257], D=0,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
+                'D=M',      # A=257, M=RAM[257], D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
+                '@SP',      # A=0,   M=RAM[0],   D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=3
+                'AM=M-1',   # A=256, M=RAM[256], D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=3
+                'M=M-D',    # A=256, M=RAM[256], D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=1
+                '@SP',      # A=0,   M=RAM[0],   D=2,  RAM[0]=256, RAM[258]=0, RAM[257]=2, RAM[256]=1
+                'M=M+1'     # A=0,   M=RAM[0],   D=2,  RAM[0]=257, RAM[258]=0, RAM[257]=2, RAM[256]=1
+            ])
         elif command == 'neg':
-            # neg                            Ex) RAM[0]=SP=258, RAM[257]=2
-            # 後から実装
-            pass
+            # neg           Ex) RAM[0]=SP=257, RAM[256]=2
+            asm_code.extend([
+                '@SP',      # A=0,   M=RAM[0],   D=0,  RAM[0]=257, RAM[257]=0, RAM[256]=2
+                'AM=M-1',   # A=256, M=RAM[256], D=0,  RAM[0]=256, RAM[257]=0, RAM[256]=2
+                'M=-M',     # A=256, M=RAM[256], D=0,  RAM[0]=256, RAM[257]=0, RAM[256]=-2
+                '@SP',      # A=0,   M=RAM[0],   D=0,  RAM[0]=256, RAM[257]=0, RAM[256]=-2
+                'M=M+1'     # A=0,   M=RAM[0],   D=0,  RAM[0]=257, RAM[257]=0, RAM[256]=-2
+            ])
         elif command == 'eq':
-            # eq                             Ex) RAM[0]=SP=258, RAM[257]=2, RAM[256]=2
-            # 後から実装
-            pass
+            # eq                              Ex) RAM[0]=SP=258, RAM[257]=y, RAM[256]=x
+            asm_code.extend([
+                '@SP',                        # A=0,      M=RAM[0],      D=0,   RAM[0]=258, RAM[257]=y, RAM[256]=x
+                'AM=M-1',                     # A=257,    M=RAM[257],    D=0,   RAM[0]=257, RAM[257]=y, RAM[256]=x
+                'D=M',                        # A=257,    M=RAM[257],    D=y,   RAM[0]=257, RAM[257]=y, RAM[256]=x
+                '@SP',                        # A=0,      M=RAM[0],      D=y,   RAM[0]=257, RAM[257]=y, RAM[256]=x
+                'AM=M-1',                     # A=256,    M=RAM[256],    D=y,   RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'D=M-D',                      # A=256,    M=RAM[256],    D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                f'@EQ_S-{self.eq_count}',     # A=EQ_S-N, M=RAM[EQ_S-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'D;JEQ'
+                # FALSEの場合(x-y!=0)
+                '@SP',                        # A=0,      M=RAM[0],      D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'A=M',                        # A=256,    M=RAM[256],    D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'M=0',                        # A=256,    M=0,           D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=0
+                f'@EQ_E-{self.eq_count}',     # A=EQ_E-N, M=RAM[EQ_E-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=0
+                '0;JMP',
+                # TRUEの場合(x-y=0)
+                f'(EQ_S-{self.eq_count})',    # A=EQ_S-N, M=RAM[EQ_S-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                '@SP',                        # A=0,      M=RAM[0],      D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'A=M',                        # A=256,    M=RAM[256],    D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'M=-1',                       # A=256,    M=-1,          D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=-1
+                # FALSEの場合のJUMP先
+                f'(EQ_E-{self.eq_count})',    # A=EQ_E-N, M=RAM[EQ_E-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=-1
+            ])
         elif command == 'gt':
-            # gt                             Ex) RAM[0]=SP=258, RAM[257]=3, RAM[256]=2
-            # 後から実装
-            pass
+            asm_code.extend([
+                '@SP',                        # A=0,      M=RAM[0],      D=0,   RAM[0]=258, RAM[257]=y, RAM[256]=x
+                'AM=M-1',                     # A=257,    M=RAM[257],    D=0,   RAM[0]=257, RAM[257]=y, RAM[256]=x
+                'D=M',                        # A=257,    M=RAM[257],    D=y,   RAM[0]=257, RAM[257]=y, RAM[256]=x
+                '@SP',                        # A=0,      M=RAM[0],      D=y,   RAM[0]=257, RAM[257]=y, RAM[256]=x
+                'AM=M-1',                     # A=256,    M=RAM[256],    D=y,   RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'D=M-D',                      # A=256,    M=RAM[256],    D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                f'@GT_S-{self.gt_count}',     # A=GT_S-N, M=RAM[GT_S-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'D;JGT'
+                # FALSEの場合(x<=y)
+                '@SP',                        # A=0,      M=RAM[0],      D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'A=M',                        # A=256,    M=RAM[256],    D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'M=0',                        # A=256,    M=0,           D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=0
+                f'@GT_E-{self.gt_count}',     # A=GT_E-N, M=RAM[GT_E-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=0
+                '0;JMP',
+                # TRUEの場合(x>y=0)
+                f'(GT_S-{self.gt_count})',    # A=GT_S-N, M=RAM[GT_S-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                '@SP',                        # A=0,      M=RAM[0],      D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'A=M',                        # A=256,    M=RAM[256],    D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'M=-1',                       # A=256,    M=-1,          D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=-1
+                # FALSEの場合のJUMP先
+                f'(GT_E-{self.gt_count})',    # A=GT_E-N, M=RAM[GT_E-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=-1
+            ])
         elif command == 'lt':
-            # lt                             Ex) RAM[0]=SP=258, RAM[257]=2, RAM[256]=3
-            # 後から実装
-            pass
+            asm_code.extend([
+                '@SP',                        # A=0,      M=RAM[0],      D=0,   RAM[0]=258, RAM[257]=y, RAM[256]=x
+                'AM=M-1',                     # A=257,    M=RAM[257],    D=0,   RAM[0]=257, RAM[257]=y, RAM[256]=x
+                'D=M',                        # A=257,    M=RAM[257],    D=y,   RAM[0]=257, RAM[257]=y, RAM[256]=x
+                '@SP',                        # A=0,      M=RAM[0],      D=y,   RAM[0]=257, RAM[257]=y, RAM[256]=x
+                'AM=M-1',                     # A=256,    M=RAM[256],    D=y,   RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'D=M-D',                      # A=256,    M=RAM[256],    D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                f'@LT_S-{self.lt_count}',     # A=LT_S-N, M=RAM[LT_S-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'D;JLT'
+                # FALSEの場合(x>=y)
+                '@SP',                        # A=0,      M=RAM[0],      D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'A=M',                        # A=256,    M=RAM[256],    D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'M=0',                        # A=256,    M=0,           D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=0
+                f'@LT_E-{self.lt_count}',     # A=LT_E-N, M=RAM[LT_E-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=0
+                '0;JMP',
+                # TRUEの場合(x<y=0)
+                f'(LT_S-{self.lt_count})',    # A=LT_S-N, M=RAM[LT_S-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                '@SP',                        # A=0,      M=RAM[0],      D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'A=M',                        # A=256,    M=RAM[256],    D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=x
+                'M=-1',                       # A=256,    M=-1,          D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=-1
+                # FALSEの場合のJUMP先
+                f'(LT_E-{self.lt_count})',    # A=LT_E-N, M=RAM[LT_E-N], D=x-y, RAM[0]=256, RAM[257]=y, RAM[256]=-1
+            ])
         elif command == 'and':
             # and                            Ex) RAM[0]=SP=258, RAM[257]=2, RAM[256]=3
             # 後から実装
@@ -79,13 +146,15 @@ class CodeWriter:
         if command == 'C_PUSH':
             if segment == 'constant':
                 # push constant x              Ex) RAM[0]=SP=256, RAM[256]=0
-                asm_code.append('@' + index)   # A=x,   M=RAM[x],   D=0,  RAM[0]=256, RAM[256]=0
-                asm_code.append('D=A')         # A=x,   M=RAM[x],   D=x,  RAM[0]=256, RAM[256]=0
-                asm_code.append('@SP')         # A=0,   M=RAM[0],   D=x,  RAM[0]=256, RAM[256]=0
-                asm_code.append('A=M')         # A=256, M=RAM[256], D=x,  RAM[0]=256, RAM[256]=0
-                asm_code.append('M=D')         # A=256, M=RMA[256], D=x,  RAM[0]=256, RAM[256]=x
-                asm_code.append('@SP')         # A=0,   M=RAM[0],   D=x,  RAM[0]=256, RAM[256]=x
-                asm_code.append('M=M+1')       # A=0,   M=RAM[0],   D=x,  RAM[0]=257, RAM[256]=x
+                asm_code.extend([
+                    '@' + index,   # A=x,   M=RAM[x],   D=0,  RAM[0]=256, RAM[256]=0
+                    'D=A',         # A=x,   M=RAM[x],   D=x,  RAM[0]=256, RAM[256]=0
+                    '@SP',         # A=0,   M=RAM[0],   D=x,  RAM[0]=256, RAM[256]=0
+                    'A=M',         # A=256, M=RAM[256], D=x,  RAM[0]=256, RAM[256]=0
+                    'M=D',         # A=256, M=RMA[256], D=x,  RAM[0]=256, RAM[256]=x
+                    '@SP',         # A=0,   M=RAM[0],   D=x,  RAM[0]=256, RAM[256]=x
+                    'M=M+1'        # A=0,   M=RAM[0],   D=x,  RAM[0]=257, RAM[256]=x
+                ])
             elif segment == 'local':
                 # push local x                       Ex) RAM[0]=SP=256, RAM[257]=2, RAM[256]=3
                 # 後から実装
